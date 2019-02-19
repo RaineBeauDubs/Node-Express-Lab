7009a6aab50f40b866a69d8426734906ec566df9
Raine Wallace <rainebeaudubs@gmail.com>
2019-02-19 16:11:43 -0500

    Got my router.js page set up and can now get posts.

---
+++ posts-router.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const db = require('./data/db');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  db
+    .find()
+    .then(posts => {
+      res
+        .status(200)
+        .json({
+          success: true, posts
+        })
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+          success: false,
+          error: 'The posts information could not be retrieved.'
+        })
+    });
+})
+
+module.exports = router
