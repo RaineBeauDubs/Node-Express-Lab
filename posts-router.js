0f1c496ffd52733f18e17db2e88b38aaa7fbd20a
Raine Wallace <rainebeaudubs@gmail.com>
2019-02-19 16:34:46 -0500

    Can now single out post by id.

--- posts-router.js
+++ posts-router.js
@@ -24,4 +24,34 @@ router.get('/', (req, res) => {
     });
 })
 
+router.get('/:id', (req, res) => {
+  db
+    .findById(req.params.id)
+    .then(post => {
+      if (post) {
+        return res
+          .status(200)
+          .json({
+            success: true,
+            post
+          })
+      } else {
+        res
+          .status(404)
+          .json({
+            success: false,
+            error: 'The post with the specified ID does not exist.'
+          })
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+          success: false,
+          error: 'The posts information could not be retrieved.'
+        })
+    });
+})
+
 module.exports = router
